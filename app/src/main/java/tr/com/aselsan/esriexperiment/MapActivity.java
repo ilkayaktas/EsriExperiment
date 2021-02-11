c93e8d4024e2348111a7e162f414dd201d9c898c
İlkay Aktaş <ilkayaktas@gmail.com>
2021-02-11 16:33:21 +0300

    Map load status is added.

--- app/src/main/java/tr/com/aselsan/esriexperiment/MapActivity.java
+++ app/src/main/java/tr/com/aselsan/esriexperiment/MapActivity.java
@@ -2,12 +2,14 @@ package tr.com.aselsan.esriexperiment;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import es.dmoral.toasty.Toasty;
 
 /**
  * Created by ilkayaktas on 2/11/21 at 1:41 PM.
@@ -26,6 +28,7 @@ public class MapActivity extends AppCompatActivity {
         mapView = findViewById(R.id.mapView);
         ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_DARK_GRAY);
         mapView.setMap(map);
+        setLoadStatus(map);
         mapView.setViewpoint(new Viewpoint(34.056295, -117.195800, 10000));
 
         mapView.addMapRotationChangedListener(m -> {
@@ -36,6 +39,39 @@ public class MapActivity extends AppCompatActivity {
             Log.d(TAG, "Map scale changed! " + m.getSource().getMapScale());
         });
 
+    }
+    
+    private void setLoadStatus(ArcGISMap map){
+        // Listener on change in map load status
+        map.addLoadStatusChangedListener(loadStatusChangedEvent -> {
+            String mapLoadStatus;
+            mapLoadStatus = loadStatusChangedEvent.getNewLoadStatus().name();
+            // map load status can be any of LOADING, FAILED_TO_LOAD, NOT_LOADED or LOADED
+            // set the status in the TextView accordingly
+            switch (mapLoadStatus) {
+                case "LOADING":
+                    Toasty.info(this, "Map Loading!", Toast.LENGTH_SHORT, true).show();
+                    break;
+
+                case "FAILED_TO_LOAD":
+                    Toasty.error(this, "Map Load Failed!", Toast.LENGTH_SHORT, true).show();
+                    break;
+
+                case "NOT_LOADED":
+                    Toasty.warning(this, "Map Not Loaded!", Toast.LENGTH_SHORT, true).show();
+                    break;
+
+                case "LOADED":
+                    Toasty.success(this, "Map Loaded!", Toast.LENGTH_SHORT, true).show();
+                    break;
+
+                default:
+                    Toasty.normal(this, "Something strange!").show();
+                    break;
+            }
+
+            Log.d(TAG, mapLoadStatus);
+        });
     }
 
     @Override
